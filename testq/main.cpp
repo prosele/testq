c2494161ef56e030152d6d49eb48d53d94582ec3
Kseniia Prokopovich <prokopovichksenia@gmail.com>
2023-09-22 00:02:59 +0300

    Initial Commit

---
+++ testq/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testq
+//
+//  Created by Ксения Прокопович on 22.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
