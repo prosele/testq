c3f18724627eb6da02a201085af205111312e77e
Kseniia Prokopovich <prokopovichksenia@gmail.com>
2024-04-18 22:02:17 +0300

    Initial Commit

---
+++ home2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  home2
+//
+//  Created by Ксения Прокопович on 17.04.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
